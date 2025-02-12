e9642f3e106ea928c3d669abe25b94bb60368532
Guanidine4336 <ayushro45fan@gmail.com>
2025-02-12 21:56:38 +0530

    Deployement fixes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,4 @@
-export const BACKEND_URL = "http://localhost:4000"
+// export const BACKEND_URL = "http://localhost:4000"
+
+
+export const BACKEND_URL = "https://coredev-ai-ce0t3142y-ayushkumar2572004-gmailcoms-projects.vercel.app/"
